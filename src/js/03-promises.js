4a4707308f56cec428d6c12f9426ebd8f8a0768e
Sebastian Lalek <chemiczq@gmail.com>
2023-09-02 19:12:52 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const value = { position, delay };
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(value);
+      } else {
+        reject(value);
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const firstDelay = parseInt(form.elements.delay.value);
+  const delayStep = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  let delay = firstDelay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay = delay + delayStep;
+  }
+});
